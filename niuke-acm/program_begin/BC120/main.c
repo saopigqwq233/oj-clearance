3721b42b47ad4144b69a33069b9b74301a85327d
SAOPIGQWQ233 <904139266@qq.com>
2023-01-23 19:26:10 +0800

    BC120

--- niuke-acm/program_begin/BC120/main.c
+++ niuke-acm/program_begin/BC120/main.c
@@ -1,25 +1,46 @@
 //BC120
+/*
 #include <stdio.h>
 int main()
 {
-    int erduan(int ,int *);//检测是否是二段
-    int n,rig;
-    scanf("%d",&n);
-    if(n==0)
-        return 0;
-    else
-    for (int i = 2; ; ++i)
+    int erduan(long int ,int ,int );//检测是否是二段
+    long int n;
+    while (scanf("%ld", &n)!=EOF)
     {
-        rig=i*n%10;
-        if(erduan(i*n,&rig))
+        if (n == 0)
+            return 0;
+        else
         {
-            printf("%d",i*n);
-            break;
+            for (int i = 2;; ++i)
+            {
+                if (erduan(i * n/10, i * n % 10,0))
+                {
+                    printf("%ld:%ld\n",n, i * n);
+                    break;
+                }
+            }
         }
     }
     return 0;
 }
-int erduan(int num,int *rig)
+int erduan(long int num,int one,int two)
 {
-
+    //printf("aa");
+    if(num%10==one&&two==0)
+        return erduan(num/10,one,two);
+    //两数分界
+    else if(num%10>one&&)
+    else
+    {
+        return 0;
+    }
+}*/
+#include "stdlib.h"
+#include "stdio.h"
+int main()
+{
+     int i=1234567;
+    char string[100];
+    printf("%s", itoa(i,string,10));
+    return 0;
 }
